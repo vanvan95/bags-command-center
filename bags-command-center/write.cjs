5f9f182c8169bce6e99f38958cba7e0be34ab87d
vanvan95 <vants195@gmail.com>
2026-04-27 20:19:25 +0700

    Debug proxy url

--- bags-command-center/write.cjs
+++ bags-command-center/write.cjs
@@ -1,15 +1,22 @@
 const fs = require('fs')
 
 fs.writeFileSync('api/bags.js', `export default async function handler(req, res) {
-  const path = req.url.replace('/api/bags', '')
+  const reqUrl = req.url || ''
+  const path = reqUrl.replace(/^\\/api\\/bags/, '')
   const url = 'https://public-api-v2.bags.fm' + path
-  const response = await fetch(url, {
-    headers: { 'x-api-key': process.env.BAGS_API_KEY }
-  })
-  const text = await response.text()
+  
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Content-Type', 'application/json')
-  res.status(response.status).send(text)
+  
+  try {
+    const response = await fetch(url, {
+      headers: { 'x-api-key': process.env.BAGS_API_KEY }
+    })
+    const text = await response.text()
+    res.status(response.status).send(text)
+  } catch(e) {
+    res.status(500).json({ error: e.message, url, reqUrl })
+  }
 }`)
 
 console.log('done!')
