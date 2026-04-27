13ee817b81aeecb2763491a9cb65ab5f5b31bf90
vanvan95 <vants195@gmail.com>
2026-04-27 18:57:46 +0700

    Update API key direct call

--- bags-command-center/write.cjs
+++ bags-command-center/write.cjs
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 fs.writeFileSync('src/api.js', `const API_BASE = 'https://public-api-v2.bags.fm/api/v1'
-const API_KEY = import.meta.env.VITE_BAGS_API_KEY || 'bags_prod_PcgAKrA3hsM2fjZCOwKa331Nh714v88YX4EkPhjwACU'
+const API_KEY = 'bags_prod_PcgAKrA3hsM2fjZCOwKa331Nh714v88YX4EkPhjwACU'
 
 export async function fetchBags(endpoint) {
   const res = await fetch(API_BASE + endpoint, {
@@ -14,4 +14,4 @@ export async function fetchBags(endpoint) {
 }
 `)
 
-console.log('api.js done!')
+console.log('done!')
