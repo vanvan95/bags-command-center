896b4976da20422cfc57035abd3db3554f3abe4a
vanvan95 <vants195@gmail.com>
2026-04-27 21:23:50 +0700

    Hardcode API key in proxy

--- bags-command-center/api/bags.js
+++ bags-command-center/api/bags.js
@@ -1,14 +1,14 @@
 export default async function handler(req, res) {
   const reqUrl = req.url || ''
-  const path = reqUrl.replace(/^\/api\/bags/, '')
+  const path = reqUrl.replace('/api/bags', '')
   const url = 'https://public-api-v2.bags.fm' + path
-  
+
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Content-Type', 'application/json')
-  
+
   try {
     const response = await fetch(url, {
-      headers: { 'x-api-key': process.env.BAGS_API_KEY || 'bags_prod_PcgAKrA3hsM2fjZCOwKa331Nh714v88YX4EkPhjwACU' }
+      headers: { 'x-api-key': 'bags_prod_PcgAKrA3hsM2fjZCOwKa331Nh714v88YX4EkPhjwACU' }
     })
     const text = await response.text()
     res.status(response.status).send(text)
